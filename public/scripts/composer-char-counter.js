e1cbef0d4c011343b15795c415f7826751056ac9
faez <faezc@hotmail.com>
2021-10-06 15:38:15 +0000

    Complete changes to numerous stylistic errors across various pages - clean up

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   console.log("the DOM is ready!");
 })
 
+// event listener handling the character counter for new tweets - updated whenever the box's input count changes and applies red styling whenn the character counter drops below 0
 $("#tweet-text").on("input", function() {
   let counter = 140 - this.value.length;
   $('.counter').text(counter);
@@ -11,7 +12,6 @@ $("#tweet-text").on("input", function() {
     $('.counter').css("color", "#545149");
   }
 })
-
 
 // // This will add a click event listener to all elements with the "button" class
 // $(".button").on("click", (event) => {
