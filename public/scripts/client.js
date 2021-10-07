baaa71d0971addf54f222077a007c1b06a514c83
faez <faezc@hotmail.com>
2021-10-07 05:01:50 +0000

    Complete tweet fetch functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,32 +3,10 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 $(document).ready(function() {
+  
+  const url = "/tweets";
 
   $("form").on("submit", function(event) {
     event.preventDefault();
@@ -38,16 +16,17 @@ $(document).ready(function() {
 
     $.ajax({
       method: "POST",
-      url: "/tweets",
+      url: url,
       data: formData
     });
   });
 
+  
   const createTweetElement = function(tweet) {
     const $tweet = $(`<article><header><div><img src=${tweet.user.avatars}>${tweet.user.name}</div><div>${tweet.user.handle}</div></header>${tweet.content.text}<footer class="tweet-footer"><div>${timeago.format(tweet.created_at)}</div><div><i class="fa-solid fa-flag"></i><i class="fa-solid fa-arrow-rotate-right"></i><i class="fa-solid fa-heart"></i></div></footer></article>`);
     return $tweet;
   };
-
+  
   const renderTweets = function(tweets) {
   
     for (const tweet of tweets) {
@@ -56,8 +35,18 @@ $(document).ready(function() {
       $('.posted-tweets').append("<br>");
     }
   };
+  
+  const loadTweets = function() {
+    $.ajax({
+      method: "GET",
+      url: url,
+    })
+    .then((tweets) => {
+      renderTweets(tweets);
+    });
+  };
 
-  renderTweets(data);
+  loadTweets();
 
 });
 
